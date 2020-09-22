2a08e17cc697099851dfaab99a883e49516bd205
petetong1989 <pete19890813@gmail.com>
2020-09-22 11:37:34 +0800

    commit

--- html/themes/adminLTE/unl_data/js/angularjs/app.js
+++ html/themes/adminLTE/unl_data/js/angularjs/app.js
@@ -187,7 +187,7 @@ app_main_unl.controller('HeaderController', ['$scope', '$http', '$location', '$r
 
     // Fix Permissions
     $scope.fixpermissions = function() {
-        html_loader = "<div id='progress-loader' style='position:fixed;bottom:16px;left:40%'><label style='float:left'>" + 
+        html_loader = "<div id='progress-loader' style='position:fixed;bottom:4%;left:40%'><label style='float:left'>" + 
         "Fix Permissions...</label><div class='loader'></div></div>";
         $(".content-wrapper").append(html_loader);
         $http({
@@ -208,7 +208,7 @@ app_main_unl.controller('HeaderController', ['$scope', '$http', '$location', '$r
 
     // IOU License
     $scope.IOUlicense = function() {
-        html_loader = "<div id='progress-loader'style='position:fixed;bottom:16px;left:40%'><label style='float:left'>" + 
+        html_loader = "<div id='progress-loader'style='position:fixed;bottom:4%;left:40%'><label style='float:left'>" + 
         "Generateing License...</label><div class='loader'></div></div>";
         $(".content-wrapper").append(html_loader);
         $http({
