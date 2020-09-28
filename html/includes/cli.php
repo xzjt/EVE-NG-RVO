1f74fa98999f06fca4cbe028240dda14c8c8d29e
petetong1989 <pete19890813@gmail.com>
2020-09-28 15:39:32 +0800

    commit

--- html/includes/cli.php
+++ html/includes/cli.php
@@ -260,7 +260,7 @@ function SetQuality($p) {
 
 	foreach ($i as $key => $value) {
 
-		if ($delay == 0 && $jitter == 0 && $loss == 0 && $bandwidth == 0) {
+		if ($delay == '0' && $jitter == '0' && $loss == '0' && $bandwidth == '0') {
 			$cmd = 'sudo tc qdisc del dev '.$value.' root';
 		} else {
 			$cmd = 'sudo tc qdisc replace dev '.$value.' root netem delay '.$delay[$key].'ms '.$jitter[$key].'ms'.' loss '.
